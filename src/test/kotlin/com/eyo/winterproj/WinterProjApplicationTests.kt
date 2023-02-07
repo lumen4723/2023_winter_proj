9b58bb2f64d42faffc0da33a42a14e0c4aa46d2e
임우영 <lmwy1214@gmail.com>
2023-02-07 15:13:58 +0900

    inital commit

---
+++ src/test/kotlin/com/eyo/winterproj/WinterProjApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eyo.winterproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WinterProjApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
