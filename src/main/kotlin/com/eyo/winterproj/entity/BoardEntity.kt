fd3f2c76884b34679957666fd802a1f30c16766c
임우영 <lmwy1214@gmail.com>
2023-02-09 16:08:55 +0900

    BoardEntity 생성

--- src/main/kotlin/com/eyo/winterproj/entity/BoardEntity.kt
+++ src/main/kotlin/com/eyo/winterproj/entity/BoardEntity.kt
@@ -1,5 +1,18 @@
 package com.eyo.winterproj.entity
 
-class BoardEntity {
 
-}
+
+import jakarta.persistence.*
+
+
+@Entity
+@Table (name = "Board")
+
+data class BoardEntity (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val article_id: Int,
+            val title: String,
+            val content: String,
+            val view: Int,
+            val user_id: Int
+
+)
