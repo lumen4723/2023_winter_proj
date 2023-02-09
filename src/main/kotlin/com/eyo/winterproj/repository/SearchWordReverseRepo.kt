5590be56bfa8be55884623bab4aaf2654a8889dc
HanDaWoon <bookpia.han@gmail.com>
2023-02-09 18:11:21 +0900

    conflicts 해결

--- src/main/kotlin/com/eyo/winterproj/repository/SearchWordReverseRepo.kt
+++ src/main/kotlin/com/eyo/winterproj/repository/SearchWordReverseRepo.kt
@@ -1,6 +1,7 @@
 package com.eyo.winterproj.repository
 
 import com.eyo.winterproj.entity.SearchWordReverseEntity
+import java.util.Optional
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
