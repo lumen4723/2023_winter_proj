6c5a4e3814a2a978ba402b20bc53dc1c1242d935
HanDaWoon <bookpia.han@gmail.com>
2023-02-09 18:14:01 +0900

    optional 2개 임포트 한거 해결

--- src/main/kotlin/com/eyo/winterproj/repository/SearchWordReverseRepo.kt
+++ src/main/kotlin/com/eyo/winterproj/repository/SearchWordReverseRepo.kt
@@ -4,9 +4,8 @@ import com.eyo.winterproj.entity.SearchWordReverseEntity
 import java.util.Optional
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.Optional
 
 @Repository
 interface SearchWordReverseRepo : CrudRepository<SearchWordReverseEntity, Long> {
-    fun findByWordId(wordId : Long) : Optional<List<SearchWordReverseEntity>>
+    fun findByWordId(wordId: Long): Optional<List<SearchWordReverseEntity>>
 }
