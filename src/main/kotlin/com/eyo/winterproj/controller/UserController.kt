a6c148cfe9d9677b2b49b9c9454ecdcc2a6e2c35
ur <dorosi150@gmail.com>
2023-02-22 17:38:58 +0900

    SendGrid 추가

--- src/main/kotlin/com/eyo/winterproj/controller/UserController.kt
+++ src/main/kotlin/com/eyo/winterproj/controller/UserController.kt
@@ -1,5 +1,6 @@
 package com.eyo.winterproj.controller
 
+import com.eyo.winterproj.service.EmailService
 import com.eyo.winterproj.service.UserService
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpSession
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 @RequestMapping("user")
-class UserController(@Autowired val userService: UserService) {
+class UserController(@Autowired val userService: UserService, @Autowired val emailService: EmailService) {
     @GetMapping("/login")
     fun login():String{
         return "login"
@@ -45,6 +47,12 @@ class UserController(@Autowired val userService: UserService) {
     fun registerP(user:RegisterRequestEntity) :String{
         userService.register(user.email, user.password, user.username)
         return "redirect:/user/login"
+    }
+    @GetMapping("/mailCheck")
+    @ResponseBody
+    fun send(user: emailRequestEntity) : String {
+        val result =  emailService.sendEmail(user.email)
+        return "TEST";
     }
 }
 
--- src/main/kotlin/com/eyo/winterproj/service/EmailService.kt
+++ src/main/kotlin/com/eyo/winterproj/service/EmailService.kt
@@ -1,4 +1,47 @@
 package com.eyo.winterproj.service
 
-class EmailService {
+import com.sendgrid.*
+import com.sendgrid.helpers.mail.Mail
+import com.sendgrid.helpers.mail.objects.Content
+import com.sendgrid.helpers.mail.objects.Email
+import com.sendgrid.helpers.mail.objects.Personalization
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Service
+import java.io.IOException
+
+@Service
+class EmailService(@Value("\${sendgrid.apiKey}") private val sendGridApiKey: String) {
+
+    @Throws(IOException::class)
+    fun sendEmail(toEmail: String) {
+
+        
+        val from = Email("test@eyo.kr")
+        val to = Email(toEmail)
+        val mail = Mail()
+        mail.from = from
+        mail.subject = "Verify your email address"
+        val p = Personalization()
+        p.addTo(to)
+//        p.addDynamicTemplateData("name", name)
+//        p.addDynamicTemplateData("confirmationCode", confirmationCode)
+        mail.personalization = listOf(p)
+
+        val classPathResource = ClassPathResource("templates/confirmation-email.html")
+        val emailTemplate = classPathResource.inputStream.bufferedReader().use { it.readText() }
+        mail.addContent(Content("text/html", emailTemplate))
+
+        val sg = SendGrid(sendGridApiKey)
+        val request = Request()
+        try {
+            request.method = Method.POST
+            request.endpoint = "mail/send"
+            request.body = mail.build()
+            val response = sg.api(request)
+            println("SendGrid status code: " + response.body)
+        } catch (ex: IOException) {
+            throw ex
+        }
+    }
 }
