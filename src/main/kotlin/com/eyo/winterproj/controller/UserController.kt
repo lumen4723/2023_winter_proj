1778888c756b1cd24ac44a442daf99efee21b688
kzinsun <kwlstjs00@gmail.com>
2023-02-23 17:33:51 +0900

    hashUtil,sms인증추가

--- src/main/kotlin/com/eyo/winterproj/controller/UserController.kt
+++ src/main/kotlin/com/eyo/winterproj/controller/UserController.kt
@@ -1,7 +1,10 @@
 package com.eyo.winterproj.controller
 
+import com.eyo.winterproj.entity.UserEntity
 import com.eyo.winterproj.service.EmailService
+import com.eyo.winterproj.service.SmsSenderService
 import com.eyo.winterproj.service.UserService
+import com.eyo.winterproj.util.HashUtil
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpSession
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("user")
-class UserController(@Autowired val userService: UserService, @Autowired val emailService: EmailService) {
+class UserController(@Autowired val userService: UserService, @Autowired val emailService: EmailService,@Autowired val smsSvc : SmsSenderService) {
     @GetMapping("/login")
     fun login():String{
         return "login"
@@ -45,15 +48,41 @@ class UserController(@Autowired val userService: UserService, @Autowired val ema
 
     @PostMapping("/signup")
     fun registerP(user:RegisterRequestEntity) :String{
-        userService.register(user.email, user.password, user.username, user.number)
-        return "redirect:/user/login"
+        if(user.validnumber==HashUtil().getHash(user.number).substring(0..6)){
+            userService.register(user.email, user.password, user.username, user.number)
+            return "redirect:/user/login"
+        }
+        else return "redirect:/user/signup?msg=%EC%9D%B8%EC%A6%9D%EC%97%90%20%EC%8B%A4%ED%8C%A8%ED%96%88%EC%8A%B5%EB%8B%88%EB%8B%A4"
     }
+
+    @GetMapping("/sendSMS")
+    @ResponseBody
+    fun sendSMS(number:String) : String {
+        if (number.length < 5) {
+            return "올바르지 않은 전화번호";
+        }
+        smsSvc.sendSMS(number, HashUtil().getHash(number).substring(0..6))
+        return "ok";
+    }
+
+    @GetMapping("/validNum")
+    @ResponseBody
+    fun validNum(number: String, validnumber:String) : String{
+        if( validnumber ==HashUtil().getHash(number).substring(0..6)){
+            return "인증이 완료되었습니다";
+        }
+        return "올바르지 않은 인증번호";
+    }
+
     @GetMapping("/mailCheck")
     @ResponseBody
     fun send(user: EmailRequestEntity) : String {
         val result =  emailService.sendEmail(user.email)
         return "TEST";
     }
+
+
+
 }
 
 data class RegisterRequestEntity(
@@ -61,6 +90,7 @@ data class RegisterRequestEntity(
     val password:String,
     val username:String,
     val number:String,
+    val validnumber: String
 )
 data class LoginRequestEntity(
     val email:String,
